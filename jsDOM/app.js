9df60cabd1a2925fb098c5c42bbdd45edfd6b706
shivusoumya <shivadatacamp@gmail.com>
2025-10-26 21:25:56 +0530

    Basic dom

---
+++ jsDOM/app.js
@@ -0,0 +1,20 @@
+
+const h1 = document.getElementById("main-title");
+// console.dir(h1);
+h1.textContent = "Hello World!";
+h1.style.color = "blue";
+h1.style.backgroundColor = "lightgray";
+h1.classList.add("title-class");
+
+const li = document.querySelector("li:last-of-type");
+li.textContent = li.textContent + "(changed)";
+
+const body = document.body;
+body.querySelector("input").value = "Changed the value";
+
+const listItemElements = document.querySelectorAll("li");
+
+for(const listItemElement of listItemElements){
+    // console.log(listItemElement.textContent);
+    console.dir(listItemElement);
+}
