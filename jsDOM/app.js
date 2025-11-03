c511cb40b0b894d0e52ddf2cb301fa12129719b4
shivusoumya <shivadatacamp@gmail.com>
2025-11-03 11:46:54 +0530

    JSDom travesing dom

--- jsDOM/app.js
+++ jsDOM/app.js
@@ -1,20 +1,27 @@
 
-const h1 = document.getElementById("main-title");
-// console.dir(h1);
-h1.textContent = "Hello World!";
-h1.style.color = "blue";
-h1.style.backgroundColor = "lightgray";
-h1.classList.add("title-class");
+// const h1 = document.getElementById("main-title");
+// // console.dir(h1);
+// h1.textContent = "Hello World!";
+// h1.style.color = "blue";
+// h1.style.backgroundColor = "lightgray";
+// h1.classList.add("title-class");
 
-const li = document.querySelector("li:last-of-type");
-li.textContent = li.textContent + "(changed)";
+// const li = document.querySelector("li:last-of-type");
+// li.textContent = li.textContent + "(changed)";
 
-const body = document.body;
-body.querySelector("input").value = "Changed the value";
+// const body = document.body;
+// body.querySelector("input").value = "Changed the value";
 
-const listItemElements = document.querySelectorAll("li");
+// const listItemElements = document.querySelectorAll("li");
 
-for(const listItemElement of listItemElements){
-    // console.log(listItemElement.textContent);
-    console.dir(listItemElement);
-}
+// for(const listItemElement of listItemElements){
+//     // console.log(listItemElement.textContent);
+//     console.dir(listItemElement);
+// }
+
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+
+console.dir(ul);
+console.dir(firstLi); 
+console.log(firstLi);  
